1e35ed642dcf3d520b2861b384e76cb1732bcf69
muriloAmachado <muriloandrademachado@gmail.com>
2024-07-28 01:38:45 -0300

    Dice Roll Game

--- DiceRollGame/RandomNumber.cs
+++ DiceRollGame/RandomNumber.cs
@@ -1,8 +1,9 @@
 ﻿class RandomNumber{
     private readonly Random _random;
+    private const int _maxNumber = 6;
 
     public RandomNumber(Random random){
         _random = random;
     }
-    public int newNumber() => _random.Next(1,7);
+    public int newNumber() => _random.Next(1,_maxNumber + 1);
 }
--- DiceRollGame/obj/Debug/net8.0/DiceRollGame.AssemblyInfo.cs
+++ DiceRollGame/obj/Debug/net8.0/DiceRollGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiceRollGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e23954e324b2ab3db94e5c5f0ea5b255c8b933db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ed806d433120aad465970fd837e54faa596f503")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiceRollGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiceRollGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

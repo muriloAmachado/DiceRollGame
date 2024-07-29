148cfd27bd4fbf657289906199a57ecdf66e9320
muriloAmachado <muriloandrademachado@gmail.com>
2024-07-29 20:20:37 -0300

    sln file

--- DiceRollGame/obj/Debug/net8.0/DiceRollGame.AssemblyInfo.cs
+++ DiceRollGame/obj/Debug/net8.0/DiceRollGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiceRollGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+989fa4d39ef4a440aa0cd85a8937e8e6745ba50a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19cc4c5aa3cf0b919104c218c86f61e5b1ab4d64")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiceRollGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiceRollGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
